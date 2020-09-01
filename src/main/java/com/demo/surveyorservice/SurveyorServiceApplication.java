5938f83fa7684f283a8d7003e0f3eb49e582c4e6
Steven <steven.itsme321@gmail.com>
2020-09-02 04:44:18 +0700

    Add bean restTemplate

--- src/main/java/com/demo/surveyorservice/SurveyorServiceApplication.java
+++ src/main/java/com/demo/surveyorservice/SurveyorServiceApplication.java
@@ -2,12 +2,18 @@ package com.demo.surveyorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SurveyorServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SurveyorServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyorServiceApplication.class, args);
+    }
 
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
