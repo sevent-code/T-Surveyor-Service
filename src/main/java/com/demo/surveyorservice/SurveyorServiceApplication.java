d336c42e5243a5f3890b7f28cecbdfe3fe0fa334
Steven <steven.mailme321@gmail.com>
2020-09-01 09:49:49 +0700

    Init Application

---
+++ src/main/java/com/demo/surveyorservice/SurveyorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.surveyorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/surveyorservice/SurveyorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.surveyorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
