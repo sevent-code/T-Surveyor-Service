4039a2075d0305833b8e1994b2b752c87660cdee
Steven <steven.itsme321@gmail.com>
2020-09-02 15:00:25 +0700

    Add sample api description

--- src/main/java/com/demo/surveyorservice/controller/SurveyorController.java
+++ src/main/java/com/demo/surveyorservice/controller/SurveyorController.java
@@ -7,7 +7,9 @@ package com.demo.surveyorservice.controller;
 
 import com.demo.surveyorservice.model.Surveyor;
 import com.demo.surveyorservice.service.SurveyorService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,8 @@ public class SurveyorController {
     @Autowired
     private SurveyorService surveyorService;
 
-    @RequestMapping("/{surveyorId}")
+    @GetMapping("/{surveyorId}")
+    @ApiOperation(value = "Find Surveyor by Id", response = Surveyor.class, notes = "Provide an id to look up from data surveyor")
     public Surveyor getOrders(@PathVariable("surveyorId") Long surveyorId) throws Exception {
         return surveyorService.findById(surveyorId);
     }
