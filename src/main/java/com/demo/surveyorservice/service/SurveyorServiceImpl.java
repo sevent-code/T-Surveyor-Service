2769a4a063864825ab4524e8723cd2c880eb1fe2
Steven <steven.itsme321@gmail.com>
2020-09-03 09:23:32 +0700

    Change return delete surveyor to void

--- src/main/java/com/demo/surveyorservice/service/SurveyorServiceImpl.java
+++ src/main/java/com/demo/surveyorservice/service/SurveyorServiceImpl.java
@@ -5,11 +5,13 @@
  */
 package com.demo.surveyorservice.service;
 
+import com.demo.surveyorservice.enums.StatusSurveyor;
 import com.demo.surveyorservice.exception.MyResourceException;
 import com.demo.surveyorservice.exception.MyResourceNotFoundException;
 import com.demo.surveyorservice.model.Surveyor;
 import com.demo.surveyorservice.repository.SurveyorRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -24,17 +26,26 @@ public class SurveyorServiceImpl implements SurveyorService {
     @Autowired
     private SurveyorRepository surveyorRepository;
 
+    @Override
+    public Surveyor createSurveyor(Surveyor surveyor) throws Exception {
+        return surveyorRepository.save(surveyor);
+    }
+    
+    @Override
+    public void deleteSurveyor(Long id) throws Exception {
+        Optional<Surveyor> optSurveyor = surveyorRepository.findById(id);
+        Surveyor surveyor = optSurveyor.get();
+        surveyor.setStatusSurveyor(StatusSurveyor.DELETED);
+        surveyorRepository.save(surveyor);
+    }
+    
+    @Override
     public Surveyor findById(Long id) {
         return surveyorRepository.findById(id).orElseThrow(() -> new MyResourceNotFoundException(id));
     }
 
     @Override
-    public Surveyor createSurveyor(Surveyor surveyor) {
-        return surveyorRepository.save(surveyor);
-    }
-
-    @Override
-    public Surveyor updateSurveyor(Long id, Surveyor requestSurveyor) {
+    public Surveyor updateSurveyor(Long id, Surveyor requestSurveyor) throws Exception {
         return surveyorRepository.findById(id).map(surveyor -> {
             surveyor = new Surveyor(requestSurveyor.getName(), requestSurveyor.getMsisdn(), requestSurveyor.getAddress(), requestSurveyor.getEmail(), requestSurveyor.getStatusSurveyor());
             return surveyorRepository.save(surveyor);
@@ -42,11 +53,11 @@ public class SurveyorServiceImpl implements SurveyorService {
     }
 
     @Override
-    public ResponseEntity<?> deleteSurveyor(Long id) {
+    public Surveyor validateOtpRegistration(Long id, String otp) throws Exception {
+        //TO-DO SEND SERVICE VALIDATE OTP
         return surveyorRepository.findById(id).map(surveyor -> {
-            surveyorRepository.delete(surveyor);
-            return ResponseEntity.ok().build();
-        }).orElseThrow(() -> new MyResourceException("Error when deleting surveyor with id " + id));
-                
+            surveyor.setStatusSurveyor(StatusSurveyor.ACTIVE);
+            return surveyorRepository.save(surveyor);
+        }).orElseThrow(() -> new MyResourceException("Error when update status surveyor with id " + id));
     }
 }
