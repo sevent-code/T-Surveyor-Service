71ee4404af7b7315dc0e266f9971987d07763b0c
Steven <steven.itsme321@gmail.com>
2020-09-03 09:25:51 +0700

    Add validate otp & delete controller

--- src/main/java/com/demo/surveyorservice/controller/SurveyorController.java
+++ src/main/java/com/demo/surveyorservice/controller/SurveyorController.java
@@ -7,6 +7,8 @@ package com.demo.surveyorservice.controller;
 
 import com.demo.surveyorservice.dto.ApiResponse;
 import com.demo.surveyorservice.dto.ReqestCreateSurveyor;
+import com.demo.surveyorservice.dto.RequestId;
+import com.demo.surveyorservice.dto.RequestValidateOtpRegistration;
 import com.demo.surveyorservice.enums.StatusSurveyor;
 import com.demo.surveyorservice.model.Surveyor;
 import com.demo.surveyorservice.service.SurveyorServiceImpl;
@@ -14,11 +16,13 @@ import com.demo.surveyorservice.utility.MyUtil;
 import io.swagger.annotations.ApiOperation;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,16 +36,34 @@ public class SurveyorController {
 
     @Autowired
     private SurveyorServiceImpl surveyorService;
-    
-    @PostMapping("/create-surveyor")
+
+    @PostMapping("/create")
     @ApiOperation(value = "Create a new surveyor", response = ApiResponse.class)
     public ApiResponse createSurveyor(@RequestBody ReqestCreateSurveyor requestData, HttpServletRequest http) throws Exception {
         System.out.println("Request From " + http.getRemoteAddr() + " on " + http.getServletPath());
         System.out.println("Data -> " + requestData.toString());
-        
+
         Surveyor surveyor = new Surveyor(requestData.getName(), requestData.getMsisdn(), requestData.getAddress(), requestData.getEmail(), StatusSurveyor.OTP_VERIFICATION);
         surveyor = surveyorService.createSurveyor(surveyor);
         return MyUtil.buildResponseWrapper("Surveyor created successfull", surveyor);
+    }
+    
+    @DeleteMapping("/delete")
+    @ApiOperation(value = "Delete surveyor - only change the status surveyor to DELETED", response = ApiResponse.class)
+    public ApiResponse deleteSurveyor(@RequestBody RequestId data, HttpServletRequest http) throws Exception {
+        System.out.println("Request From " + http.getRemoteAddr() + " on " + http.getServletPath());
+        surveyorService.deleteSurveyor(data.getId());
+        return MyUtil.buildResponseWrapper("Success", null);
+    }
+
+    @PostMapping("/validate-otp-registration")
+    @ApiOperation(value = "Validate otp registration that send after fill the form", response = ApiResponse.class)
+    public ApiResponse validateOtpRegistration(@RequestBody RequestValidateOtpRegistration requestData, HttpServletRequest http) throws Exception {
+        System.out.println("Request From " + http.getRemoteAddr() + " on " + http.getServletPath());
+        System.out.println("Data -> " + requestData.toString());
+        
+        Surveyor surveyor = surveyorService.validateOtpRegistration(requestData.getId(), requestData.getOtp());
+        return MyUtil.buildResponseWrapper("Success", surveyor);
     }
     
     
