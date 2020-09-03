5cad35198c2276c49c9ab808376b17c4f0a88e0b
Steven <steven.itsme321@gmail.com>
2020-09-03 09:23:24 +0700

    Change return delete surveyor to void

--- src/main/java/com/demo/surveyorservice/service/SurveyorService.java
+++ src/main/java/com/demo/surveyorservice/service/SurveyorService.java
@@ -14,7 +14,9 @@ import org.springframework.http.ResponseEntity;
  * @author Steven Raylianto K.
  */
 public interface SurveyorService {
-    Surveyor createSurveyor(Surveyor surveyor);
-    Surveyor updateSurveyor(Long id, Surveyor requestSurveyor);
-    ResponseEntity<?> deleteSurveyor(Long id);
+    Surveyor createSurveyor(Surveyor surveyor) throws Exception;
+    void deleteSurveyor(Long id) throws Exception;
+    Surveyor findById(Long id) throws Exception;
+    Surveyor updateSurveyor(Long id, Surveyor requestSurveyor) throws Exception;
+    Surveyor validateOtpRegistration(Long id, String otp) throws Exception;
 }
